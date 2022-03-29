5cdc7291fa81df5ff2f8b3922d7dc61c24f4a98b
blanket <fuckblanket@protonmail.com>
2022-03-29 17:17:15 -0400

    changed file name and switched to reinterpret_cast

--- plus_binder/keyboard.cc
+++ plus_binder/keyboard.cc
@@ -17,7 +17,7 @@ void CKeyboard::OnLowLevelHook(int nCode, WPARAM wParam, LPARAM lParam) {
 	std::scoped_lock(m_KeyMutex);
 
 	/* get KBDLLHOOKSTRUCT info */
-	auto pKbdll = (KBDLLHOOKSTRUCT*)lParam;
+	auto pKbdll = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
 
 	VirtualKey_t* pBindableKey = &m_VirtualKeys[pKbdll->vkCode];
 
@@ -32,14 +32,11 @@ void CKeyboard::OnLowLevelHook(int nCode, WPARAM wParam, LPARAM lParam) {
 
 
 VirtualKey_t CKeyboard::GetKeyInfo(const std::uint32_t uVkCode) {
-	/* note: yes, making a copy each time is slow. this is thread safe tho (: */
 	std::scoped_lock(m_KeyMutex);
 	return m_VirtualKeys[uVkCode];
 }
 
-
 void CKeyboard::SetCallback(const std::uint32_t uVkCode, const std::function<void(VirtualKey_t, std::uint32_t)>& pCallbackFunction) {
-	/* note: this isnt really that safe or good, just use it smartly! */
 	std::scoped_lock(m_KeyMutex);
 	m_VirtualKeys[uVkCode].m_KeyCallback = pCallbackFunction;
 }
@@ -61,7 +58,7 @@ void CKeyboard::Initialize() {
 	std::thread thdMsgLoop([]() {
 
 		/* initialize key hook */
-		hkKbdHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)WinCallbackKBDLL, 0, 0);
+		hkKbdHook = SetWindowsHookEx(WH_KEYBOARD_LL, reinterpret_cast<HOOKPROC>(WinCallbackKBDLL), 0, 0);
 
 		/* message loop */
 		{
